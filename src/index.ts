a1c2a5d19661f12e1a95d0e5d23f7ac87837d310
Lucas Navajas <lucastobiasnavajas@gmail.com>
2024-02-23 16:05:54 -0300

    Mongoose

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
-import { createPool } from "mysql2/promise";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -16,19 +16,14 @@ app.use(compression());
 app.use(cookieParser());
 app.use(bodyParser.json());
 
-const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'gamelibrary',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
-
-
 const server = http.createServer(app);
 
 server.listen(8000, () => {
     console.log("Server running on localhost 8000");
 });
+
+const MONGO_URL = "mongodb+srv://lucas:lucas@cluster0.mjjxpge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));

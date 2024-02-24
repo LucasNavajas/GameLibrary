d17115c79ca2169962dc7683e88c5c534db55858
Lucas Navajas <lucastobiasnavajas@gmail.com>
2024-02-24 11:42:57 -0300

    Auth

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import router from "./router";
 import mongoose from "mongoose";
 
 const app = express();
@@ -27,3 +28,5 @@ const MONGO_URL = "mongodb+srv://lucas:lucas@cluster0.mjjxpge.mongodb.net/?retry
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use("/", router());
